84ee4ac3feac9ac24aba5825492ff244ab5c434f
Neil Fraser <fraser@google.com>
2021-08-02 12:04:25 -0700

    Update update.sh

--- raspberry/update.sh
+++ raspberry/update.sh
@@ -1,6 +1,7 @@
 # Fetch a new copy of all Raspberry Pi software from GitHub.
 # Despite no-cache, githubusercontent caches for 5 minutes.
-wget --no-cache -O /home/pi/player.py https://raw.githubusercontent.com/NeilFraser/glockenspiel/master/raspberry/crontab.txt
-wget --no-cache -O /home/pi/player.py https://raw.githubusercontent.com/NeilFraser/glockenspiel/master/raspberry/player.py
-wget --no-cache -O /home/pi/player.py https://raw.githubusercontent.com/NeilFraser/glockenspiel/master/raspberry/westminster.py
-wget --no-cache -O /home/pi/player.py https://raw.githubusercontent.com/NeilFraser/glockenspiel/master/raspberry/update.sh
+wget --no-cache -r -O /home/pi/player.py https://raw.githubusercontent.com/NeilFraser/glockenspiel/master/raspberry/crontab.txt
+wget --no-cache -r -O /home/pi/player.py https://raw.githubusercontent.com/NeilFraser/glockenspiel/master/raspberry/player.py
+wget --no-cache -r -O /home/pi/player.py https://raw.githubusercontent.com/NeilFraser/glockenspiel/master/raspberry/westminster.py
+wget --no-cache -r -O /home/pi/player.py https://raw.githubusercontent.com/NeilFraser/glockenspiel/master/raspberry/update.sh
+chmod +x player.py westminster.py update.sh
